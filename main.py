774ef5cfe9771a6987509512503004c4b4238155
MicheLeVeilleur <60694308+MicheLeVeilleur@users.noreply.github.com>
2020-03-02 18:02:01 +0100

    ajout commentaires main

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ fenetre.blit(fond, (0,0))
 pygame.display.set_icon(pygame.image.load(var.img_icon))#icone de la fenetre
 affFenetre = True
 
-pygame.mixer.music.load("background_music.mp3")
+pygame.mixer.music.load("background_music.mp3")#musique de fond
 #pygame.mixer.music.play()
 
 ##debut des evenements
@@ -27,35 +27,35 @@ while affFenetre:
     clock = pygame.time.Clock()
     clock.tick(60)
     for event in pygame.event.get():    #On parcours la liste de tous les événements reçus
-        if event.type == QUIT:
+        if event.type == QUIT:#si on appuie sur la croix de la fenetre 
             affFenetre = False      #On arrête la boucle
-        if event.type == MOUSEBUTTONDOWN and event.button == 1:# # clic gauche : sprite bouge
-            try:var.playerList[0].clic(event)
+        if event.type == MOUSEBUTTONDOWN and event.button == 1:# # clic gauche
+            try:var.playerList[0].clic(event)#le 1er player de playerlist enregistre nouv destination si la liste est pas vide
             except:pass
-        if event.type == MOUSEBUTTONDOWN and event.button == 2:##clic molette : sprite lance un missile
-            try:var.playerList[0].shootClic(event)
+        if event.type == MOUSEBUTTONDOWN and event.button == 2:##clic molette
+            try:var.playerList[0].shootClic(event)#le 1er player de playerlist tire si la liste est pas vide
             except:pass
-        if event.type == KEYDOWN and event.key == K_SPACE:
+        if event.type == KEYDOWN and event.key == K_SPACE:#on crée un nouv player et on le met dans playerlist
             classes.utility.respawn()
     
     for objet in var.refreshList:#boucle de mouvement
-        objet.angle = (classes.utility.getBearing((objet.x,objet.y),(objet.xDest,objet.yDest))+90)%360
-        classes.utility.rotate(objet,objet.angle)
-        objet.goTick(objet.xDest,objet.yDest)
+        objet.angle = (classes.utility.getBearing((objet.x,objet.y),(objet.xDest,objet.yDest))+90)%360#calcul de l'angle de l'ojet par rapport à sa dest
+        classes.utility.rotate(objet,objet.angle)# on le tourne de cet angle
+        objet.goTick(objet.xDest,objet.yDest)#on on lance la fonction tick de l'objet
     
     var.hitList = []
 
     for objet in var.refreshList:
-        objet.rect = objet.sprite.get_rect(center = (objet.x,objet.y))
+        objet.rect = objet.sprite.get_rect(center = (objet.x,objet.y))#on remet les co de l'objet à son centre(évite bug de rotate)
     
     for objet in var.refreshList:
         for objet2 in var.refreshList:#boucle de test hitbox
-            if objet.rect.colliderect(objet2.rect) and objet != objet2:
-                if not(type(objet)==classes.Missile and objet.timeAlive < 3) and not(type(objet2)==classes.Missile and objet2.timeAlive < 3):
+            if objet.rect.colliderect(objet2.rect) and objet != objet2:#si 1 touche 2 et 1 différent de 2
+                if not(type(objet)==classes.Missile and objet.timeAlive < 3) and not(type(objet2)==classes.Missile and objet2.timeAlive < 3):#si 1 et 2 sont pas des missiles dans la phase d'invincibilité
                     print("col")
-                    var.hitList.append(objet) 
+                    var.hitList.append(objet) #on ajoute l'objet 1 à la hitList
     
-    for objet in var.hitList[::-1]:#boucle delete
+    for objet in var.hitList[::-1]:#boucle delete(on déréférence les objets de toute liste pour pouvoir les supprimer)
         var.refreshList.remove(objet)
         var.hitList.remove(objet)
         try:var.playerList.remove(objet)
