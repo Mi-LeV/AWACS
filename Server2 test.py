d33e9dce27dd14dcb8897e5de175ed086698a456
Goenae <61237031+Goenae@users.noreply.github.com>
2020-03-20 10:06:57 +0100

    Add files via upload
    Simplification du code et ajout de commentaires.

--- Server2 test.py
+++ Server2 test.py
@@ -13,28 +13,27 @@ serveur_lance = True
 clients_connectes = []
 
 while serveur_lance:
-    connexion_demandees, wlist, xlist = select.select([connexion_principale], [], [], 0.05)
+    connexion_demandees, wlist, xlist = select.select([connexion_principale], [], [], 0.05) #select renvoie la liste des clients qui ont un message à réceptionner
 
     for connexion in connexion_demandees:
         connexion_avec_client, infos_connexion = connexion_principale.accept()
-        print(connexion_avec_client,infos_connexion)
         clients_connectes.append(connexion_avec_client)
     
     clients_a_lire = []
     try:
-        clients_a_lire, wlist, xlist = select.select(clients_connectes, [], 0.05)
-    except:
-        select.error
+        clients_a_lire, wlist, xlist = select.select(clients_connectes, [], [], 0.05) #wlist est la liste des sockets en attente d'etre écrits, xlist est la liste des sockets en attente d'une erreur
+    except select.error:
+        pass
     else:
         for client in clients_a_lire:
             msg_recu = client.recv(1024)
-            msg_recu = msg_recu.decode()
+            msg_recu = msg_recu.decode() #transforme une chaine bytes en chaine str
             client.send(b"recu")
             if msg_recu == "disconnect":
                 serveur_lance = False
-            msg_a_envoyer = msg_recu
-            msg_a_envoyer = msg_a_envoyer.encode()
-            client.send(msg_a_envoyer)
+            position = msg_recu.encode() #transforme une chaine str en chaine bytes         
+
+            client.send(position) #la chaine doit etre en bytes pour que send fonctionne
 
 
 print("disconnected")
